e1879c015072ad048ba3f827a514a231072611b4
cabaf <fabiancabaheilbron@gmail.com>
2016-02-02 16:21:52 +0300

    Added quicksort algo.

---
+++ quick_sort.py
@@ -0,0 +1,43 @@
+import unittest
+
+class QuickSortTest(unittest.TestCase):
+    def test_quick_sort(self):
+        l = [7,5,9,11,6]
+        l_sorted = [5,6,7,9,11]
+        self.assertListEqual(quick_sort(l), l_sorted)
+
+    def test_quick_sort_duplicates(self):
+        l = [1,1,1,0,0,0,2]
+        l_sorted = [0,0,0,1,1,1,2]
+        self.assertListEqual(quick_sort(l), l_sorted)
+
+    def test_quick_sort_intercalary(self):
+        l = [1,3,2,4,5,7,6,8]
+        l_sorted = [1,2,3,4,5,6,7,8]
+        self.assertListEqual(quick_sort(l), l_sorted)
+
+    def test_quick_sort_single(self):
+        l = [999]
+        l_sorted = [999]
+        self.assertListEqual(quick_sort(l), l_sorted)
+
+def quick_sort(x):
+    """Sorts an input list using the quick sort algorithm.
+       Worst case running time is O(n^2). Expected running time is O(nlgn).
+       It does not require extra space --> O(1) space.       
+    """
+    if len(x) <= 1:
+        return x
+    pivot = x[0]
+    pivot_less, pivot_equal, pivot_greater = [], [], []
+    for element in x:
+        if element < pivot:
+            pivot_less.append(element)
+        elif element == pivot:
+            pivot_equal.append(element)
+        else:
+            pivot_greater.append(element)
+    return quick_sort(pivot_less) + pivot_equal + quick_sort(pivot_greater)
+
+if __name__ == '__main__':
+    unittest.main()
