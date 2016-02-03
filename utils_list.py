3270545cc840e35f1df840a9870576b6186a4e1a
cabaf <fabiancabaheilbron@gmail.com>
2016-02-03 03:18:00 +0300

    Find most repeated value in constant time (relaxed constraint).

--- utils_list.py
+++ utils_list.py
@@ -34,5 +34,36 @@ def max_difference(x):
             max_val = element
     return max_val - min_val
 
+"""
+Given a sorted array of size N of int32, find an element that 
+repeats > ceil(N/2) times. Your algo may assume that there will 
+be always such element.
+"""
+class FindMostRepeatedTest(unittest.TestCase):
+
+    def test_find_most_repeated_even(self):
+        x = [1, 1, 1, 1, 1, 0, 0, 0]
+        val = 1
+        self.assertEqual(find_most_repeated(x), val)
+
+    def test_find_most_repeated_odd(self):
+        x = [1, 1, 1, 1, 1, 0, 0]
+        val = 1
+        self.assertEqual(find_most_repeated(x), val)
+
+    def test_find_most_repeated_min_size(self):
+        x = [1, 1]
+        val = 1
+        self.assertEqual(find_most_repeated(x), val)
+
+def find_most_repeated(x):
+    if len(x) <= 1:
+        raise ValueError('x must contain an element that repeats > ceil(N/2)')
+    half_index = len(x) / 2
+    if x[half_index] == x[0]:
+        return x[0]
+    else:
+        return x[-1]
+
 if __name__ == '__main__':
     unittest.main()
